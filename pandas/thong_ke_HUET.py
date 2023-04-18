84439d3da6b18bb9adb648af9ecbb970a36036f9
Phạm Phước Bảo Tín <baotinphamphuoc@gmail.com>
2023-04-18 07:49:48 +0700

    Update thong_ke_HUET.py

--- pandas/thong_ke_HUET.py
+++ pandas/thong_ke_HUET.py
@@ -15,10 +15,17 @@ df.groupby(by='Lớp').agg({'Thu nhập': 'mean'})
 df.groupby(['Lớp'])['Thu nhập'].mean() # trung bình thu nhập theo lớp
 # thông tin thu nhập max, min từng lớp
 df.groupby(['Lớp'])['Thu nhập'].max()
+df.groupby(['Lớp'])['Thu nhập'].min()
 g=df.groupby(['Lớp'])
+
 def hien_theo_lop(g):
     for grade, item in g:
         print('Lớp:'+str(grade))
         print( str(item),'\n\n')
-a=df.groupby(['Lớp'])['Thu nhập'].min()
-print(a)
+
+# tùy chọn đọc từ hàng thứ đầu tiền đến hàng n
+df.head(10)
+# đọc từ hàng 2 đến hàng 9
+df.loc[2:9]
+# ngoài ra có thể đọc số hàng tùy chọn mà ko cần phải đọc toàn bộ file, skiprows : bo qua 2 dong dau bat dau doc tu dong 3 voi so luong 7 dong
+df1=pd.read_excel("E:\\PTDL\\pandas\\sv_HUET.xlsx",skiprows=3,nrows=7)
